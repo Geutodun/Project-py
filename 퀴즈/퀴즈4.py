cb385775fec9a4e0fdc08d6ceeb17d31feee4676
Geutodun <67596860+Geutodun@users.noreply.github.com>
2021-05-11 19:09:10 +0900

    Create 퀴즈4.py

---
+++ 퀴즈/퀴즈4.py
@@ -0,0 +1,34 @@
+from random import *
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+x = sample(a, 4)
+
+
+print("-- 당첨자 발표 --")
+print("치킨 당첨자".format(x[0]))
+
+print("커피 당첨자".format(x[1:]))
+print("-- 축하합니다 --")
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+from random import *
+
+users = range(1, 21)
+
+users = list(users)
+
+winners = sample(users, 4)
+
+shuffle(users)
+
+print("-- 당첨자 발표 --")
+
+print("치킨 당첨자".format(winners[0]))
+
+print("커피 당첨자".format(winners[1:]))
+
+print("-- 축하합니다 --")
