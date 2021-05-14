8feba4ced154c63e8cb223aed33e6554d20896d9
Geutodun <67596860+Geutodun@users.noreply.github.com>
2021-05-14 15:21:56 +0900

    Create 퀴즈6.py

---
+++ 퀴즈/퀴즈6.py
@@ -0,0 +1,10 @@
+def st(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+
+height = 175
+gender = "남자"
+weight = round(st(height / 100, gender), 2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
